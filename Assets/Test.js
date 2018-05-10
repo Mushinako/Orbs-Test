d920ad79c35dd9ba32cd6fd989036c8d5c1595f1
Mushinako <ridoedee@gmail.com>
2018-05-10 12:48:16 -0700

    Initialize
    Upload and README

---
+++ Assets/Test.js
@@ -0,0 +1,193 @@
+class DistanceSquare {
+    constructor() {
+        this.x1 = Math.random();
+        this.x2 = Math.random();
+        this.y1 = Math.random();
+        this.y2 = Math.random();
+        this.d = Math.sqrt(
+            (this.x1-this.x2) * (this.x1-this.x2)
+            + (this.y1-this.y2) * (this.y1-this.y2)
+        );
+    }
+}
+
+class DistanceCircle {
+    constructor() {
+        this.t1 = Math.random() * 2 * Math.PI;
+        this.t2 = Math.random() * 2 * Math.PI;
+        this.x1 = Math.cos(this.t1);
+        this.x2 = Math.cos(this.t2);
+        this.y1 = Math.sin(this.t1);
+        this.y2 = Math.sin(this.t2);
+        this.d = Math.sqrt(
+            (this.x1-this.x2) * (this.x1-this.x2)
+            + (this.y1-this.y2) * (this.y1-this.y2)
+        );
+    }
+}
+
+var ds = ['Nothing to see'];
+var worker;
+var m, w;
+// var running = false;
+
+dgebi = s => document.getElementById(s);
+
+function calcSquare(pts) {
+    var sm = 0;
+    var d = [];
+    for (let _ = 0; _ < pts; _++) {
+        let tmp = new DistanceSquare();
+        d.push(tmp);
+        sm += tmp.d;
+    }
+    postMessage([sm, d]);
+}
+
+function calcCircle(pts) {
+    var sm = 0;
+    var d = [];
+    for (let _ = 0; _ < pts; _++) {
+        let tmp = new DistanceCircle();
+        d.push(tmp);
+        sm += tmp.d;
+    }
+    postMessage([sm, d]);
+}
+
+function square(wasm, work, points) {
+    pre = Math.sqrt(2) / 15 + Math.log(1+Math.sqrt(2)) / 3 + 2 / 15;
+    dgebi('quest').innerHTML = 'Average distance of 2 points in a unit square';
+    dgebi('prediction').innerHTML = `Prediction: \\(\\frac{1}{15}\\sqrt{2}+\\frac{1}{3}\\ln{(1+\\sqrt{2})}+\\frac{2}{15}\\approx${pre}\\)`;
+    MathJax.Hub.Queue(['Typeset',MathJax.Hub]);
+
+    if (wasm) {} else if (work) {
+        let code = calcSquare.toString();
+        code = (`var pts = ${points};\n`
+            + DistanceSquare.toString()
+            + code.substring(code.indexOf('{')+1, code.lastIndexOf('}')));
+        let blob = new Blob([code], {type: 'application/javascript'});
+        worker = new Worker(URL.createObjectURL(blob));
+
+        worker.onmessage = message => {
+            let msg = message.data;
+            let sum = msg[0];
+            ds = msg[1];
+
+            let res = sum / points;
+            dgebi('result').innerHTML = `Average with ${points} points: ${res}`;
+            let diff = (res - pre) / pre * 100;
+            dgebi('diff').innerHTML = `Difference: ${diff}%;`;
+        };
+    } else {
+        let sum = 0;
+
+        for (let _ = 0; _ < points; _++) {
+            let tmp = new DistanceSquare();
+            ds.push(tmp);
+            sum += tmp.d;
+        }
+
+        let res = sum / points;
+        dgebi('result').innerHTML = `Average with ${points} points: ${res}`;
+        let diff = (res - pre) / pre * 100;
+        dgebi('diff').innerHTML = `Difference: ${diff}%;`;
+    }
+}
+
+function circle(wasm, work, points) {
+    pre = 4 / Math.PI;
+    dgebi('quest').innerHTML = 'Average distance of 2 points on a unit circle';
+    dgebi('prediction').innerHTML = `Prediction: \\(\\frac{4}{\\pi}\\approx${pre}\\)`;
+    MathJax.Hub.Queue(['Typeset',MathJax.Hub]);
+
+    if (wasm) {} else if (work) {
+        let code = calcCircle.toString();
+        code = (`var pts = ${points};\n`
+            + DistanceCircle.toString()
+            + code.substring(code.indexOf('{')+1, code.lastIndexOf('}')));
+        let blob = new Blob([code], {type: 'application/javascript'});
+        worker = new Worker(URL.createObjectURL(blob));
+
+        worker.onmessage = message => {
+            let msg = message.data;
+            let sum = msg[0];
+            ds = msg[1];
+
+            let res = sum / points;
+            dgebi('result').innerHTML = `Average with ${points} points: ${res}`;
+            let diff = (res - pre) / pre * 100;
+            dgebi('diff').innerHTML = `Difference: ${diff}%;`;
+        };
+    } else {
+        let sum = 0;
+
+        for (let _ = 0; _ < points; _++) {
+            let tmp = new DistanceCircle();
+            ds.push(tmp);
+            sum += tmp.d;
+        }
+
+        let res = sum / points;
+        dgebi('result').innerHTML = `Average with ${points} points: ${res}`;
+        let diff = (res - pre) / pre * 100;
+        dgebi('diff').innerHTML = `Difference: ${diff}%;`;
+    }
+}
+
+function calc(mode) {
+    dgebi('result').innerHTML = 'Plotting points...';
+    dgebi('diff').innerHTML = '';
+    var points = 1e6;
+
+    switch (mode) {
+        case 'square':
+            square(m, w, points);
+            break;
+        case 'circle':
+            circle(m, w, points);
+            break;
+        default:
+            throw new RuntimeException('Nothing here!');
+    }
+}
+
+function check() {
+    m = Boolean(undefined);
+    w = Boolean(window.Worker);
+    var methods = '';
+
+    if (m) {
+        dgebi('wasm').innerHTML = `
+            Your browser supports WebAssembly!
+            Simulation would run faster than the alternative XD
+        `;
+        methods += '<button onclick="m = true;">WebAssembly</button>\n';
+    } else {
+        dgebi('wasm').innerHTML = `
+            Your browser does not support WebAssembly!
+            That\'s OK, just that the simulation will run slower :D
+        `;
+    }
+
+    if (w) {
+        dgebi('worker').innerHTML = `
+            Your browser supports Web Workers!
+            Your browser should not freeze during large simulations XD
+        `;
+        methods += '<button onclick="m = false; w = true;">Web Worker</button>\n';
+    } else {
+        dgebi('worker').innerHTML = `
+            Your browser does not support Web Workers!
+            That\'s OK, just that the browser may freeze during large simulations :D
+        `;
+    }
+
+    if (methods.length) {
+        dgebi('method').innerHTML = `
+            You can choose differet methods to use!
+            Click on a method then the one you want to calculate!
+        `;
+        dgebi('methods').innerHTML = methods + '<button onclick="m = false; w = false;">Neither</button>';
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Orbs-Test
+# Orbs-Test <img align="right" src="Assets/icon.png" width="300">
+This simulation runs a designated number of points and averages them for their
+distances.
+
+## Progress
+- [ ] Webpage Structure
+    - [x] Basic Layout
+    - [ ] Links
+    - [ ] Credits and Minutiae
+- [ ] Unit Square
+    - [x] Prediction
+    - [x] JavaScript
+    - [x] Web Workers
+    - [ ] WebAssembly
+- [ ] Unit Circle
+    - [ ] Prediction
+    - [x] JavaScript
+    - [x] Web Workers
+    - [ ] WebAssembly
+- [ ] Unit Disk
+    - [ ] Prediction
+    - [ ] JavaScript
+    - [ ] Web Workers
+    - [ ] WebAssembly
+
+## License
+This work is distributed under CC BY-SA 4.0. See license for more details
